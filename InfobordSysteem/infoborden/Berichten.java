090fdfffe90caf3235430b25c275dfcabfb3ff14
SJHelmhout <54943946+SJHelmhout@users.noreply.github.com>
2023-01-25 17:06:28 +0100

    Reducing unit complexity Berichten.setRegels

--- InfobordSysteem/infoborden/Berichten.java
+++ InfobordSysteem/infoborden/Berichten.java
@@ -5,11 +5,14 @@ import java.util.HashMap;
 import org.codehaus.jackson.map.ObjectMapper;
 public class Berichten {
 
-	private HashMap<String,Integer> laatsteBericht = new HashMap<String,Integer>();
-	private HashMap<String,JSONBericht> infoBordRegels = new HashMap<String,JSONBericht>();
+	private final HashMap<String,Integer> laatsteBericht = new HashMap<>();
+	private final HashMap<String,JSONBericht> infoBordRegels = new HashMap<>();
 	private int hashValue;
 	private boolean refresh;
 	private String[] infoTekstRegels;
+
+	private int aantalRegels;
+	private int[] aankomsttijden;
 	
 	public void nieuwBericht(String incoming) {
 		try {
@@ -30,40 +33,41 @@ public class Berichten {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void setRegels(){
-		String[] infoTekst={"--1--","--2--","--3--","--4--","leeg"};
-		int[] aankomsttijden=new int[5];
-		int totaalTijden=0;
-		int aantalRegels = 0;
+		aantalRegels = 0;
+		String[] infoTekst = new String[]{"--1--", "--2--", "--3--", "--4--", "leeg"};
+		aankomsttijden=new int[5];
 		if(!infoBordRegels.isEmpty()){
 			for(String busID: infoBordRegels.keySet()){
 				JSONBericht regel = infoBordRegels.get(busID);
-				int dezeTijd=regel.getAankomsttijd();
-				String dezeTekst=regel.getInfoRegel();
-				int plaats=aantalRegels;
-				for(int i=aantalRegels;i>0;i--){
-					if(dezeTijd<aankomsttijden[i-1]){
-						aankomsttijden[i]=aankomsttijden[i-1];
-						infoTekst[i]=infoTekst[i-1];
-						plaats=i-1;
-					}
-				}
-				aankomsttijden[plaats]=dezeTijd;
-				infoTekst[plaats]=dezeTekst;
-				if(aantalRegels<4){
-					aantalRegels++;
-				}
+				addRegel(regel, infoTekst);
 			}
 		}
-		refresh = checkRepaint(aantalRegels, aankomsttijden);
+		refresh = checkRepaint();
 		infoTekstRegels = infoTekst;
 	}
+
+	public void addRegel(JSONBericht regel, String[] infoTekst){
+		int dezeTijd=regel.getAankomsttijd();
+		String dezeTekst=regel.getInfoRegel();
+		int plaats=aantalRegels;
+		for(int i=aantalRegels;i>0;i--){
+			if(dezeTijd<aankomsttijden[i-1]){
+				aankomsttijden[i]=aankomsttijden[i-1];
+				infoTekst[i]=infoTekst[i-1];
+				plaats=i-1;
+			}
+		}
+		aankomsttijden[plaats]=dezeTijd;
+		infoTekst[plaats]=dezeTekst;
+		if(aantalRegels<4){ aantalRegels++; }
+	}
 	
-	private boolean checkRepaint(int aantalRegels, int[] aankomsttijden){
+	private boolean checkRepaint(){
 		int totaalTijden=0;
-		for(int i=0; i<aantalRegels;i++){
-			totaalTijden+=aankomsttijden[i];
+		for(int i=0; i<this.aantalRegels;i++){
+			totaalTijden+=this.aankomsttijden[i];
 		}
 		if(hashValue!=totaalTijden){
 			hashValue=totaalTijden;
